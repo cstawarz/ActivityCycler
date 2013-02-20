a0d1e50f514d67e103c444158fe59ef17ffdef22
Christopher Stawarz <cstawarz@mit.edu>
2013-02-19 21:28:43 -0500

    More work on GUI

--- controller.py
+++ controller.py
@@ -6,27 +6,50 @@ class Controller(QtGui.QWidget):
     def __init__(self):
         super(Controller, self).__init__()
 
-        self.layout = QtGui.QVBoxLayout()
-        self.setLayout(self.layout)
-
-        intervals_group = QtGui.QGroupBox()
-        self.intervals = QtGui.QFormLayout()
-        intervals_group.setLayout(self.intervals)
-        
-        self.layout.addWidget(intervals_group)
-        self.intervals.addWidget(QtGui.QPushButton('Add interval'))
-
         self.active = False
 
+        self.layout = QtGui.QVBoxLayout()
+        self.create_intervals_group()
+        self.create_controls_group()
+        self.setLayout(self.layout)
+
+    def create_intervals_group(self):
+        self.intervals = QtGui.QFormLayout()
+
+        pb = QtGui.QPushButton('Add interval')
+        pb.clicked.connect(self.add_interval)
+        self.intervals.addRow(pb)
+
         self.add_interval()
         self.add_interval()
+
+        group = QtGui.QGroupBox()
+        group.setLayout(self.intervals)
+        self.layout.addWidget(group)
+
+    def create_controls_group(self):
+        buttons = QtGui.QHBoxLayout()
+
+        run = QtGui.QPushButton('Run')
+        buttons.addWidget(run)
+
+        stop = QtGui.QPushButton('Stop')
+        stop.setDisabled(True)
+        buttons.addWidget(stop)
+
+        reset = QtGui.QPushButton('Reset')
+        buttons.addWidget(reset)
+
+        group = QtGui.QGroupBox()
+        group.setLayout(buttons)
+        self.layout.addWidget(group)
  
     def add_interval(self):
-        spinbox = QtGui.QSpinBox()
-        spinbox.setRange(0, 10800)
-        spinbox.setSuffix(' s')
+        sb = QtGui.QSpinBox()
+        sb.setRange(0, 10800)
+        sb.setSuffix(' s')
         label = ('A' if self.active else 'Ina') + 'ctive:'
-        self.intervals.insertRow(self.intervals.rowCount()-1, label, spinbox)
+        self.intervals.insertRow(self.intervals.rowCount()-1, label, sb)
         self.active = not self.active
 
 
